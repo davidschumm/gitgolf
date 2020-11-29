f538c4c68a988f7d67916fd96eb1201248c6b99f
davidschumm <46762601+davidschumm@users.noreply.github.com>
2020-11-28 22:43:17 -0500

    added database, 1st functional crud app

--- app.py
+++ app.py
@@ -1,9 +1,78 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+#import smtplib
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+# Initialize the database
+db = SQLAlchemy(app)
+
+# create db model
+class Friends(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+	# Create a function to return a string when we add something
+	def __repr__(self):
+		return '<Name %r>' % self.id
+
 
 
 subscribers = []
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+	friend_to_delete = Friends.query.get_or_404(id)
+
+	try:
+		db.session.delete(friend_to_delete)
+		db.session.commit()
+		return redirect('/friends')
+
+	except:
+		return "There was a problem deleting that friend"
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+	friend_to_update = Friends.query.get_or_404(id)
+	if request.method == "POST":
+		friend_to_update.name = request.form['name']
+		try:
+			db.session.commit()
+			return redirect('/friends')
+		except:
+			return "There was a problem updating that friend"
+
+	else:
+		return render_template('update.html', friend_to_update=friend_to_update)
+
+
+
+@app.route('/friends', methods=['POST', 'GET'])
+def friends():
+	title = "!GitGolf Users!"
+
+	if request.method == "POST":
+		friend_name = request.form['name']
+		new_friend = Friends(name=friend_name)
+
+
+		# Push to database
+		try:
+			db.session.add(new_friend)
+			db.session.commit()
+			return redirect('friends')
+		except:
+			return "There was an error adding friend"
+
+	else:
+		friends = Friends.query.order_by(Friends.date_created)
+		return render_template("friends.html", title=title, friends=friends)
+
+
+
 
 @app.route('/')
 def index():
@@ -28,6 +97,12 @@ def form():
 	last_name = request.form.get("last_name")
 	email = request.form.get("email")
 	password = request.form.get("password")
+
+	#message = "You have been subscribed to my email newsletter"
+	#server = smtplib.SMTP("smtp.gmail.com", 587)
+	#server.starttls()
+	#server.login("myemail@gmail.com", "password")
+	#server.sendmail("myemail@gmail.com", email, message)
 
 	if not first_name or not last_name or not email or not password:
 		error_statement = "All Form Fields Required..."
